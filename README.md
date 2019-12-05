582e6ba1dff99b505990d431e1cceb7a4a032aa7
Victor Parmar <vic@smalldata.tech>
2019-12-05 13:24:33 +0100

    f formatting

--- README.md
+++ README.md
@@ -8,40 +8,42 @@ A mongoose plugin that deletes provided paths on a document and its sub document
 
 When instantiated, this plugin adds a static method `stripPaths()` to your schema. Upon invoking this method, it loops over the provided paths in the `options.paths` variable and sets them to `undefined`. If there are any nested sub documents (via a single nested schema or embedded within an array), the `stripPaths()` method of all respective sub documents is also invoked. This works for nested subdocuments as well as documents added via a populate query.
 
-Note that there will be no validation done upon invoking the `stripPaths()` method, which means that the document could be in an invalid state. As of version 0.0.2 `stripPaths()` also returns a reference to the document for easy chaining. 
+Note that there will be no validation done upon invoking the `stripPaths()` method, which means that the document could be in an invalid state. As of version 0.0.2 `stripPaths()` also returns a reference to the document for easy chaining.
 
 This functionality should ideally only be used to return the document for further processing.
 
 ## Usage
 
 For example, given schemas as follows:
+
 ```javascript
-let mongoose                = require('mongoose');
-let mongooseStripPaths      = require('mongoose-strip-paths').mongooseStripPaths;
-let Schema                  = mongoose.Schema;
+let mongoose = require("mongoose");
+let mongooseStripPaths = require("mongoose-strip-paths").mongooseStripPaths;
+let Schema = mongoose.Schema;
 
 let PostSchema = new Schema({
-  title: String, 
+  title: String,
   message: String,
   fieldToStrip: String
 });
 
-PostSchema.plugin(mongooseStripPaths, {paths: ['fieldToStrip']});
+PostSchema.plugin(mongooseStripPaths, { paths: ["fieldToStrip"] });
 
 let UserSchema = new Schema({
   username: String,
-  createdAt: {type: Date, required: true},
+  createdAt: { type: Date, required: true },
   posts: [Post]
 });
 
-// note that if you add 'posts', then the posts field will be removed, 
+// note that if you add 'posts', then the posts field will be removed,
 // also note that after stripping 'createdAt' mongoose validation will fail on trying to save it
-UserSchema.plugin(mongooseStripPaths, {paths: ['createdAt']}); 
+UserSchema.plugin(mongooseStripPaths, { paths: ["createdAt"] });
 
-let User = mongoose.model('User', UserSchema);
+let User = mongoose.model("User", UserSchema);
 ```
 
 Calling the strip fields method on a user will result in the following document:
+
 ```javascript
 let user = new User({
   username: 'u1',
@@ -105,7 +107,7 @@ let userObj = user.stripPaths().toObject();
 
 0. Install dependencies with `npm install` and [install mongo](http://docs.mongodb.org/manual/installation/) if you don't have it yet.
 1. Start mongo with `mongod`.
-2. Run tests with `npm test`. Additionally you can pass your own mongodb uri as an environment variable if you would like to test against your own database, for e.g. `URI='mongodb://username:password@localhost/mongoose-strip-paths-test' npm test`
+1. Run tests with `npm test`. Additionally you can pass your own mongodb uri as an environment variable if you would like to test against your own database, for e.g. `URI='mongodb://username:password@localhost/mongoose-strip-paths-test' npm test`
 
 ## Publishing
 
--- test/test-mongoose-strip-paths.js
+++ test/test-mongoose-strip-paths.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
+const expect = require("chai").expect;
 
-const Promise = require('bluebird');
-const mongoose = require('mongoose');
+const Promise = require("bluebird");
+const mongoose = require("mongoose");
 mongoose.Promise = Promise;
 const Schema = mongoose.Schema;
 
-const mongooseStripPaths = require('../lib/mongoose-strip-paths').mongooseStripPaths;
+const mongooseStripPaths = require("../lib/mongoose-strip-paths").mongooseStripPaths;
 
-const uri = process.env.URI || 'mongodb://localhost/mongoose-strip-paths-test';
+const uri = process.env.URI || "mongodb://localhost/mongoose-strip-paths-test";
 
 // setup schemas
 
@@ -18,7 +18,7 @@ let AddressSchema = new Schema({
   city: String
 });
 
-AddressSchema.plugin(mongooseStripPaths, { paths: ['street', '_id', '__v'] });
+AddressSchema.plugin(mongooseStripPaths, { paths: ["street", "_id", "__v"] });
 
 let PostSchema = new Schema({
   title: String,
@@ -26,7 +26,7 @@ let PostSchema = new Schema({
   fieldToStrip: String
 });
 
-PostSchema.plugin(mongooseStripPaths, { paths: ['fieldToStrip'] });
+PostSchema.plugin(mongooseStripPaths, { paths: ["fieldToStrip"] });
 
 let UserSchema = new Schema({
   username: String,
@@ -35,38 +35,35 @@ let UserSchema = new Schema({
   address: AddressSchema
 });
 
-// note that if you add 'posts', then the posts field will be removed, 
+// note that if you add 'posts', then the posts field will be removed,
 // also note that after stripping 'createdAt' mongoose validation will fail on trying to save it
-UserSchema.plugin(mongooseStripPaths, { paths: ['createdAt'] });
+UserSchema.plugin(mongooseStripPaths, { paths: ["createdAt"] });
 
-let User = mongoose.model('User', UserSchema);
+let User = mongoose.model("User", UserSchema);
 
 let UserGroupSchema = new Schema({
-  users: [{ type: Schema.ObjectId, ref: 'User' }]
+  users: [{ type: Schema.ObjectId, ref: "User" }]
 });
 
 UserGroupSchema.plugin(mongooseStripPaths, { paths: [] });
 
-let UserGroup = mongoose.model('UserGroup', UserGroupSchema);
+let UserGroup = mongoose.model("UserGroup", UserGroupSchema);
 
 // test harness
 
-describe('mongoose-strip-paths plugin', () => {
-
+describe("mongoose-strip-paths plugin", () => {
   before(() => {
-    return mongoose.connect(uri)
-      .then(() => {
-        return mongoose.connection.db.dropDatabase();
-      });
+    return mongoose.connect(uri).then(() => {
+      return mongoose.connection.db.dropDatabase();
+    });
   });
 
   after(() => {
-    mongoose.connection.close()
+    mongoose.connection.close();
   });
 
-  describe('setup', () => {
-    it('should not error when no options provided', () => {
-
+  describe("setup", () => {
+    it("should not error when no options provided", () => {
       // given
       let RandomSchema = new Schema({
         field: String
@@ -76,39 +73,38 @@ describe('mongoose-strip-paths plugin', () => {
       RandomSchema.plugin(mongooseStripPaths);
 
       // then
-      let Random = mongoose.model('Random', RandomSchema);
+      let Random = mongoose.model("Random", RandomSchema);
       let random = new Random({
-        field: 'foo'
+        field: "foo"
       });
 
       random.stripPaths();
     });
   });
 
-  describe('statics', () => {
-    it('should strip paths on object', () => {
-
+  describe("statics", () => {
+    it("should strip paths on object", () => {
       // given
       let user = new User({
-        username: 'u1',
+        username: "u1",
         createdAt: new Date(),
         posts: [],
         address: {
-          street: '1 street',
-          city: 'Montréal'
+          street: "1 street",
+          city: "Montréal"
         }
       });
 
       user.posts.push({
-        title: 'first post',
-        message: 'hello',
-        fieldToStrip: 'some internal data'
+        title: "first post",
+        message: "hello",
+        fieldToStrip: "some internal data"
       });
 
       user.posts.push({
-        title: 'second post',
-        message: 'world',
-        fieldToStrip: 'some more internal data'
+        title: "second post",
+        message: "world",
+        fieldToStrip: "some more internal data"
       });
 
       // when
@@ -119,38 +115,36 @@ describe('mongoose-strip-paths plugin', () => {
       expect(strippedUser.posts[0].fieldToStrip).to.be.undefined;
       expect(strippedUser.posts[1].fieldToStrip).to.be.undefined;
       expect(strippedUser.address.street).to.be.undefined;
-
     });
   });
 
-
-  describe('db', () => {
-    it('should strip paths on a populated object', () => {
-
+  describe("db", () => {
+    it("should strip paths on a populated object", () => {
       // given
       let user = new User({
-        username: 'u1',
+        username: "u1",
         createdAt: new Date(),
         posts: [],
         address: {
-          street: '1 street',
-          city: 'Montréal'
+          street: "1 street",
+          city: "Montréal"
         }
       });
 
       user.posts.push({
-        title: 'first post',
-        message: 'hello',
-        fieldToStrip: 'some internal data'
+        title: "first post",
+        message: "hello",
+        fieldToStrip: "some internal data"
       });
 
       user.posts.push({
-        title: 'second post',
-        message: 'world',
-        fieldToStrip: 'some more internal data'
+        title: "second post",
+        message: "world",
+        fieldToStrip: "some more internal data"
       });
 
-      return user.save()
+      return user
+        .save()
         .then(savedUser => {
           let userGroup = new UserGroup({
             users: [savedUser._id]
@@ -159,8 +153,7 @@ describe('mongoose-strip-paths plugin', () => {
           return userGroup.save();
         })
         .then(savedUserGroup => {
-          return UserGroup.findById(savedUserGroup._id)
-            .populate('users');
+          return UserGroup.findById(savedUserGroup._id).populate("users");
         })
         .then(foundUserGroup => {
           // when
@@ -176,5 +169,4 @@ describe('mongoose-strip-paths plugin', () => {
         });
     });
   });
-
 });
